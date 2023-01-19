992317931038fe8fcaf09ffa1632363b8ad265e7
Nanoskript <96655713+nanoskript@users.noreply.github.com>
2023-01-19 09:50:31 -0500

    style: keep declare global at top of file

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,11 +2,6 @@ export {};
 
 /// <reference types="cypress" />
 
-Cypress.Commands.add("login", (password: string) => {
-  cy.visit("/login");
-  cy.get("[name=password]").type(password, { log: false });
-  cy.get("[type=submit]").contains("Submit").click();
-});
 declare global {
   namespace Cypress {
     interface Chainable {
@@ -14,3 +9,9 @@ declare global {
     }
   }
 }
+
+Cypress.Commands.add("login", (password: string) => {
+  cy.visit("/login");
+  cy.get("[name=password]").type(password, { log: false });
+  cy.get("[type=submit]").contains("Submit").click();
+});
