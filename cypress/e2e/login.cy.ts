8dcda8463a3022e43b689add99eaf74f191d88f0
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2023-01-21 23:36:23 +1300

    test: remove cypress.env.json dependency

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -6,7 +6,7 @@ describe("Login page", () => {
   });
 
   it("allows user to login", () => {
-    cy.get("[name=password]").type(Cypress.env("password"), { log: false });
+    cy.get("[name=password]").type(Cypress.env("PASSWORD"), { log: false });
     cy.get("[type=submit]").contains("Submit").click();
     cy.get("[type=submit]").contains("Logout");
   });
@@ -18,7 +18,7 @@ describe("Login page", () => {
   });
 
   it("allows user to logout", () => {
-    cy.login(Cypress.env("password"));
+    cy.login(Cypress.env("PASSWORD"));
     cy.get("[type=submit]").contains("Logout").click();
     cy.get('a[href*="login"]');
   });
